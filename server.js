5abd41f3a34edfcfead4ba31aad8c4fe06c51c11
ashu9584 <ashu.chaubey123@gmail.com>
2016-11-10 18:57:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(session({
     secret: 'someRandomSecretValue',
     cookie: { maxAge: 1000 * 60 * 60 * 24 }
 }));
-console.log(crypto.gethashes());
+console.log(crypto.getHashes());
 var pool = new pg.Pool(config);
 function quiztemplate(ques)
 {   
