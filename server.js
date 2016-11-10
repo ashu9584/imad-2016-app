6a46c61b8487975fffd4284f6f6c67e68ccba1ad
ashu9584 <ashu.chaubey123@gmail.com>
2016-11-10 22:37:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -212,7 +212,7 @@ function quiztemplate(ques)
     var score = $('<p>',{id: 'question'});
     var numCorrect = 0;
     var request = new XMLHttpRequest();
-        console.log('success);
+        console.log('success');
         // Capture the response and store it in a variable
         request.onreadystatechange = function () {
           if (request.readyState === XMLHttpRequest.DONE) {
